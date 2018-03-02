e885ead154f799fd4adf22a6e0363f5e92a21829
dtiezzi <dtiezzi@usp.br>
2018-03-02 17:24:58 -0300

    dnaToProtein added!

---
+++ python/dnaToProtein.py
@@ -0,0 +1,38 @@
+def dnaToProtein(seq):
+    seq = seq.upper()
+    rna= []
+    for i in seq:
+        if i =='T':
+            i= 'U'
+            rna.append(i)
+        else:
+            rna.append(i)
+        rnaseq= ''.join(rna)
+    prot= []
+    for i in range(0,len(rnaseq),3):
+        codon= rnaseq[i:i+3]
+        aa= map[codon]
+        prot.append(aa)        
+    return ''.join(prot)
+
+
+map = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"s", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
+
+dna= input("Type or paste your DNA sequence:\n")
+
+print(dnaToProtein(dna))
