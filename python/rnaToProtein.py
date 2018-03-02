1902680770875f230bbf12f08a809237cc6dfec7
Daniel Tiezzi <dtiezzi@Daniels-MacBook-Pro.local>
2018-03-02 16:59:32 -0300

    rnaToProtein done

---
+++ python/rnaToProtein.py
@@ -0,0 +1,30 @@
+def rnaToProtein(seq):
+    seq = seq.upper()
+    prot= []
+    for i in range(0,len(seq),3):
+        codon= seq[i:i+3]
+        aa= map[codon]
+        prot.append(aa)        
+    return ''.join(prot)
+
+
+map = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"s", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
+
+rna= input("Type or paste your RNA sequence:")
+
+print(rnaToProtein(rna))
